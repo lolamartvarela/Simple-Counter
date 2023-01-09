5f1e33018c0faba959aaa611e91c7118ead2eb91
lolamartvarela <luciamvarela@outlook.com>
2023-01-09 15:37:11 +0000

    2

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Cards from "./Cards.jsx";
+//include images into your bundle
 
 //create your first component
 const SecondsCounter = (props) => {
-    return (
-    <>
-      <h1>Contador: {props.numero1}</h1>
-      <Cards/>
-    </>
-    );
+	return (
+<> 
+<Cards/>
+<div>
+				<h1>Contador: {props.numero6}{props.numero5}{props.numero4}{props.numero3}{props.numero2}{props.numero1} </h1>
+			</div>
+
+</>
+	);
 };
 
 export default SecondsCounter;
-
 
 
 
